67967278ec9f5058845e86d62dcd604f508c72ad
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2022-10-31 18:13:08 +0000

    first commit

---
+++ scripts/content.js
@@ -0,0 +1,8 @@
+var styles = `
+    * {
+      border-radius: 0px !important;
+    }
+`
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
